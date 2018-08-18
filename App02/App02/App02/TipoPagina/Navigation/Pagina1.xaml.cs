1512f49bff72695c114f942b90822395a75e5019
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-18 17:27:23 -0300

    Criação do modal

--- App02/App02/App02/TipoPagina/Navigation/Pagina1.xaml.cs
+++ App02/App02/App02/TipoPagina/Navigation/Pagina1.xaml.cs
@@ -5,16 +5,21 @@ using Xamarin.Forms.Xaml;
 namespace App02.TipoPagina.Navigation
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Pagina1 : ContentPage
-	{
-		public Pagina1 ()
-		{
-			InitializeComponent ();
-		}
+    public partial class Pagina1 : ContentPage
+    {
+        public Pagina1()
+        {
+            InitializeComponent();
+        }
 
-        private void MudarParaPagina2 (object sender, EventArgs eventArgs)
+        private void MudarParaPagina2(object sender, EventArgs eventArgs)
         {
             Navigation.PushAsync(new Pagina2());
+        }
+
+        private void ChamarModal(object sender, EventArgs eventArgs)
+        {
+            Navigation.PushModalAsync(new Modal());
         }
     }
 }
--- App02/App02/App02/TipoPagina/Navigation/Pagina2.xaml.cs
+++ App02/App02/App02/TipoPagina/Navigation/Pagina2.xaml.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace App02.TipoPagina.Navigation
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Pagina2 : ContentPage
-	{
-		public Pagina2 ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina2 : ContentPage
+    {
+        public Pagina2()
+        {
+            InitializeComponent();
+        }
+    }
 }

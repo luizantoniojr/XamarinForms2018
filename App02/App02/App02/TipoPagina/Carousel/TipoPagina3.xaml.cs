4f384f1f377668b1629add5e887d76b15b59b056
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-18 16:38:36 -0300

    Criação do carousel para o navigation.

--- App02/App02/App02/TipoPagina/Carousel/TipoPagina3.xaml.cs
+++ App02/App02/App02/TipoPagina/Carousel/TipoPagina3.xaml.cs
@@ -1,20 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using App02.TipoPagina.Navigation;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace App02.TipoPagina.Carousel
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TipoPagina3 : ContentPage
-	{
-		public TipoPagina3 ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TipoPagina3 : ContentPage
+    {
+        public TipoPagina3()
+        {
+            InitializeComponent();
+        }
+
+        private void MudarPagina(object sender, EventArgs eventArgs)
+        {
+            App.Current.MainPage = new Pagina1();
+        }
+    }
 }

7e97eaf0b8ea587df70e22bfa85f0e36e49d9acc
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-18 19:08:09 -0300

    Ajuste no maste detail

--- App02/App02/App02/TipoPagina/Master/Master.xaml.cs
+++ App02/App02/App02/TipoPagina/Master/Master.xaml.cs
@@ -16,14 +16,17 @@ namespace App02.TipoPagina.Master
         public void MudaConteudo3(object sender, EventArgs args)
         {
             Detail = new Conteudo3();
+            IsPresented = false;
         }
         public void MudaConteudo2(object sender, EventArgs args)
         {
             Detail = new Conteudo2();
+            IsPresented = false;
         }
         public void MudaConteudo(object sender, EventArgs args)
         {
             Detail = new Conteudo();
+            IsPresented = false;
         }
 
     }

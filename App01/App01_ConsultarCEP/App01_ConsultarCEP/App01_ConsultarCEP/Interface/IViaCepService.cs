fb065945b1260e22b97fa0b38d3125d6f6d75fe2
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-18 15:37:59 -0300

    Correções

--- App01/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Interface/IViaCepService.cs
+++
@@ -1,9 +0,0 @@
-﻿using App01_ConsultarCEP.Model;
-
-namespace App01_ConsultarCEP.Interface
-{
-    public interface IViaCepService
-    {
-        Resultado BuscarEnderecoViaCep(Cep cep);
-    }
-}
--- App01/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Model/Resultado.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App01_ConsultarCEP.Model
-{
-    public class Resultado
-    {
-        public bool EhValido { get { return string.IsNullOrEmpty(MensagemErro); } }
-        public string MensagemErro { get; set; }
-        public ViaCepRetorno Endereco { get; set; }
-    }
-}

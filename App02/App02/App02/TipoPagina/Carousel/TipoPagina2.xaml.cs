8c40ecdf13ec50811b7b271a1862dfbc4acc6e0a
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-18 18:10:41 -0300

    ,

--- App02/App02/App02/TipoPagina/Carousel/TipoPagina2.xaml.cs
+++ App02/App02/App02/TipoPagina/Carousel/TipoPagina2.xaml.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace App02.TipoPagina.Carousel
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TipoPagina2 : ContentPage
-	{
-		public TipoPagina2 ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TipoPagina2 : ContentPage
+    {
+        public TipoPagina2()
+        {
+            InitializeComponent();
+        }
+    }
 }

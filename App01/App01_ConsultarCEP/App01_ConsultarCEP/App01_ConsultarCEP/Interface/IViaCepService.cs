2ef257cbbdd63dea7266f1f6438ca3036936ec7b
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-18 15:21:12 -0300

    .

---
+++ App01/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Interface/IViaCepService.cs
@@ -0,0 +1,9 @@
+﻿using App01_ConsultarCEP.Model;
+
+namespace App01_ConsultarCEP.Interface
+{
+    public interface IViaCepService
+    {
+        Resultado BuscarEnderecoViaCep(Cep cep);
+    }
+}
---
+++ App01/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Model/Cep.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace App01_ConsultarCEP.Model
+{
+    public struct Cep
+    {
+        private readonly string _valor;
+        public readonly bool EhValido;
+
+        public Cep(string valor)
+        {
+            _valor = valor;
+
+            if (valor == null)
+            {
+                EhValido = false;
+                return;
+            }
+
+            var Rgx = new Regex("^\\d{5}-\\d{3}$");
+
+            if (!Rgx.IsMatch(valor))
+                EhValido = false;
+            else
+                EhValido = true;
+        }
+
+        public static implicit operator Cep(string valor) => new Cep(valor);
+
+        public override string ToString() => _valor;
+    }
+}

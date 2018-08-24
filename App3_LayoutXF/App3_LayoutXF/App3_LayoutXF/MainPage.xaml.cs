45495bfae8acfbc108f5379c978d07dd59df5e26
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-23 21:41:51 -0300

    aula 45

---
+++ App3_LayoutXF/App3_LayoutXF/App3_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using App3_LayoutXF.Layouts.Absolute;
+using App3_LayoutXF.Layouts.Grid;
+using App3_LayoutXF.Layouts.Relative;
+using App3_LayoutXF.Layouts.Scroll;
+using App3_LayoutXF.Layouts.Stack;
+using System;
+using Xamarin.Forms;
+
+namespace App3_LayoutXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AbrirAbsolute(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new AbsolutePage());
+        }
+
+        private void AbrirGrid(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new GridPage());
+        }
+
+        private void AbrirRelative(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new RelativePage());
+        }
+
+        private void AbrirScroll(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new ScrollPage());
+        }
+
+        private void AbrirStack(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new StackPage());
+        }
+    }
+}

91ff691ca94ff07ebde24cf48d07fa371522549e
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-25 18:53:08 -0300

    Aula 62

---
+++ App5/App5/App5/Menu/Master.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+    }
+}

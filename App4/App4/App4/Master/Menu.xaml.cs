eb8aa3194d51155246aacee22e6d4b63968a3ab4
Luiz Antônio da Silva Júnior <silvajuniorluizantonio@gmail.com>
2018-08-25 14:50:03 -0300

    Aula 55

--- App4/App4/App4/Master/Menu.xaml.cs
+++ App4/App4/App4/Master/Menu.xaml.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using App4.Pages;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace App4.Master
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Menu : ContentPage
-	{
-		public Menu ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        public void IrParaPerfil(object sender, EventArgs arags)
+        {
+            Navigation.PushAsync(new Pefil());
+        }
+            
+        public void IrParaSobre(object sender, EventArgs arags)
+        {
+            Navigation.PushAsync(new About());
+        }
+    }
 }
